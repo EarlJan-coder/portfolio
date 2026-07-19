545764b8613c10d1c3ddbebb4fcb38c6be590c1b
EarlJan-coder <earljhonamalatag2@gmail.com>
2026-07-19 21:58:19 +0800

    scroll reveal fix

--- components/ui/ScrollReveal.tsx
+++ components/ui/ScrollReveal.tsx
@@ -1,10 +1,10 @@
 "use client";
 
 import * as React from "react";
-import { motion, useInView } from "framer-motion";
+import { motion, useInView, HTMLMotionProps } from "framer-motion";
 import { cn } from "@/lib/utils";
 
-export interface ScrollRevealProps extends React.HTMLAttributes<HTMLDivElement> {
+export interface ScrollRevealProps extends HTMLMotionProps<"div"> {
   children: React.ReactNode;
   delay?: number;
   once?: boolean;
